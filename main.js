c29f028249d9c849f6f55e2e9984b8c1bcbad017
Nishant Joshi <nishantjoshi@Nishants-MacBook-Pro.local>
2024-02-04 18:36:11 +0545

    feat : remote connection status and design change

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-const {app, BrowserWindow, ipcMain} = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const axios = require("axios");
 const io = require("socket.io-client");
 
 // const Autolaunch = require("auto-launch");
 
 
-function initSocket() {
-  const socket = io("https://ci-uat-bak.naasasecurities.com.np");
+function initSocket(window) {
+  const socket = io("https://ci-uat-bak.naasasecurities.com.np",
+    {
+      transportOptions: {
+        polling: {
+          extraHeaders: {
+            "Authorization": "remote-command"
+          }
+        }
+      }
+    });
+  socket.on("remote-command", (payload) => {
+    window.webContents.send("remote-command", payload);
+    // console.log("remote command execution");
+  });
+
   socket.on("connect", () => {
-    console.log("Connected");
+    window.webContents.send("remote-connection-status", "Connected");
+  });
+
+  socket.on("disconnect", () => {
+    window.webContents.send("remote-connection-status", "Not Connected");
   })
 }
 
@@ -29,6 +47,9 @@ function createWindow() {
   });
 
   win.loadFile('index.html');
+  
+  initSocket(win);
+  // win.webContents.openDevTools();
 
   setInterval(() => {
     axios.get('https://us-central1-wft-desktop-app.cloudfunctions.net/api/update-status')
@@ -43,12 +64,12 @@ function createWindow() {
 
 app.whenReady().then(() => {
   createWindow();
-  initSocket();
+
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow(); 
-      initSocket();
-     
+      createWindow();
+      
+
     }
   });
 });
--- renderer.js
+++ renderer.js
@@ -1,9 +1,19 @@
 const  {updateSystemInfo}  = require("./systemInfo");
 const {updateSystemLog}  = require("./systemLog");
+const {ipcRenderer} = require("electron");
+
+ipcRenderer.on("remote-command", (event, arg) => {
+  console.log(arg.command);
+});
+
+ipcRenderer.on("remote-connection-status", (event, arg) => {
+  document.getElementById("remote-connection-status").innerText = `Remote Connection Status : ${arg}`
+})
+
 
 const updateOnlineStatus = () => {
 
-  document.getElementById('status').innerHTML = navigator.onLine ? 'online' : 'offline'
+  document.getElementById('status').innerHTML = navigator.onLine ? 'Internet Connection Status : Online' : 'Internet Connection Status : Offline'
   updateSystemLog()
   updateSystemInfo()
 
